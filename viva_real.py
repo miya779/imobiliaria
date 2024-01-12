94439b82cfb7fd45fd257f2625f0010f58f970c6
Adriana Miyazaki <adriana@bravado.com.br>
2024-01-12 11:28:08 -0300

    viva real

--- viva_real.py
+++ viva_real.py
@@ -3,15 +3,18 @@ import requests, re, time
 import pandas as pd
 from selenium import webdriver
 from urllib.parse import urljoin
+from datetime import datetime
 
 
 #selenium Firefox driver
 driver = webdriver.Firefox()
 
 # O link deve estar ordenado por preço ascendente para garantir que o script irá funcionar
-url1 = "https://www.vivareal.com.br/venda/sp/atibaia/bairros/caetetuba/apartamento_residencial/?pagina="
-url2 = "#onde=Brasil,S%C3%A3o%20Paulo,Atibaia,Bairros,Caetetuba,,,,BR%3ESao%20Paulo%3ENULL%3EAtibaia%3EBarrios%3ECaetetuba,,,&ordenar-por=preco:ASC" # site
-
+#url1 = "https://www.vivareal.com.br/venda/sp/sao-paulo/zona-sul/jardim-paulista/apartamento_residencial/?pagina="
+#url2 = "#onde=,S%C3%A3o%20Paulo,S%C3%A3o%20Paulo,Zona%20Sul,Jardim%20Paulista,,,neighborhood,BR%3ESao%20Paulo%3ENULL%3ESao%20Paulo%3EZona%20Sul%3EJardim%20Paulista,,,&ordenar-por=preco:ASC&preco-ate=700000&tipos=apartamento_residencial,cobertura_residencial"
+url1 = "https://www.vivareal.com.br/venda/sp/atibaia/bairros/jardim-paulista/casa_residencial/?pagina="
+url2 = "#onde=,São Paulo,Atibaia,Bairros,Jardim Paulista,,,neighborhood,BR>Sao Paulo>NULL>Atibaia>Barrios>Jardim Paulista,,,&ordenar-por=preco:ASC"
+#url2 = "#onde=,S%C3%A3o%20Paulo,Atibaia,Bairros,Jardim%20Paulista,,,neighborhood,BR%3ESao%20Paulo%3ENULL%3EAtibaia%3EBarrios%3EJardim%20Paulista,,,;,S%C3%A3o%20Paulo,Atibaia,Bairros,Vila%20Giglio,,,neighborhood,BR%3ESao%20Paulo%3ENULL%3EAtibaia%3EBarrios%3EVila%20Giglio,,,;,S%C3%A3o%20Paulo,Atibaia,Bairros,Jardim%20Jaragua,,,neighborhood,BR%3ESao%20Paulo%3ENULL%3EAtibaia%3EBarrios%3EJardim%20Jaragua,,,;,S%C3%A3o%20Paulo,Atibaia,Bairros,Jardim%20Das%20flores,,,neighborhood,BR%3ESao%20Paulo%3ENULL%3EAtibaia%3EBarrios%3EJardim%20Das%20flores,,,;,S%C3%A3o%20Paulo,Atibaia,Bairros,Jardim%20do%20Lago,,,neighborhood,BR%3ESao%20Paulo%3ENULL%3EAtibaia%3EBarrios%3EJardim%20do%20Lago,,,;,S%C3%A3o%20Paulo,Atibaia,Bairros,Nova%20Gardenia,,,neighborhood,BR%3ESao%20Paulo%3ENULL%3EAtibaia%3EBarrios%3ENova%20Gardenia,,,;,S%C3%A3o%20Paulo,Atibaia,Bairros,Jardim%20Sui%C3%A7a,,,neighborhood,BR%3ESao%20Paulo%3ENULL%3EAtibaia%3EBarrios%3EJardim%20Suica,,,;,S%C3%A3o%20Paulo,Atibaia,Bairros,Vila%20Thais,,,neighborhood,BR%3ESao%20Paulo%3ENULL%3EAtibaia%3EBarrios%3EVila%20Thais,,,&ordenar-por=preco:ASC&tipos=casa_residencial,condominio_residencial,sobrado_residencial,,,"
 
 current_page = 1
 last_page = 1
@@ -22,7 +25,7 @@ while current_page <= last_page:
     print("\nPage: " + str(current_page))
     url_pagination = url1 + str(current_page) + url2
     driver.get(url_pagination)
-    time.sleep(4)
+    time.sleep(20)
     response = driver.execute_script("return document.getElementsByTagName('html')[0].innerHTML")
     soup = BeautifulSoup(response, 'html.parser')
     links = soup.find_all('a',class_="property-card__content-link")
@@ -56,5 +59,7 @@ while current_page <= last_page:
 driver.close()
 
 df = df.sort_values(by="valor_m2")
-df.to_csv('viva_real.csv', index=False)    
+now = datetime.now()
+now = now.strftime("%Y-%m-%d-%H:%M:%S")
+df.to_csv('viva_real' + now + '.csv', index=False)    
 
